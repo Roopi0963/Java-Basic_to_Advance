932f49124bfd599fe05f1070d87a596e13c9bc80
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2024-12-19 23:37:53 +0530

    updated files

---
+++ Java-Programming/src/Operators/Operators.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        int salary;
+        salary= 300000;
+        int salaryIncrement;
+        salaryIncrement = salary + (salary * 10)/100;
+        System.out.println("Salary increment is "+salaryIncrement);
+
+    }
+}
