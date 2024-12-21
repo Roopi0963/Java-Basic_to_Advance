b26380dd2305c8c69d90ef77494974826e213335
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2024-12-21 18:17:49 +0530

    fundamentals

---
+++ java-programming/src/com/java/training/Loops/Loops.java
@@ -0,0 +1,12 @@
+package com.java.Loops;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        short val;
+        val = 10;
+        for (int i = 5; i < val; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ java-programming/src/com/java/training/Switch_Cases/Switch_case.java
@@ -0,0 +1,20 @@
+package com.java.Switch_Cases;
+
+public class Switch_case {
+    public static void main(String[] args) {
+        String day = "saturday";
+
+        switch(day)
+        {
+            case "sunday","saturday" :
+                System.out.println("8 am");
+                break;
+            case "monday" :
+                System.out.println("7 am");
+                break;
+            default :
+                System.out.println("6 am");
+
+        }
+    }
+}
