de70090e6d5dacad652e2dcf8c2fe10db126e3d6
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2024-12-22 22:20:10 +0530

    Class on customer details

---
+++ Java-Programming/src/com/java/Oops/ClassandObject/Class_and_object/Customer.java
@@ -0,0 +1,14 @@
+package com.java.training.oops.ClassandObject;
+
+public class Customer {
+    // state (data)
+    // Instance variables , the data will be stored in heap memory
+
+    int id;
+    String name;
+    String contactNo;
+    String email;
+
+
+
+}
