debebd37994ac9b511a538b4c1c165a7c4daebb9
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmai.com>
2024-12-23 22:48:01 +0530

    Constructors and static methods

---
+++ Java-Programming/src/com/java/oops/constructors/Constructors/main.java
@@ -0,0 +1,15 @@
+package com.java.oops.constructors.Constructors;
+
+public class main {
+    public static void main(String[] args) {
+
+
+        Customer customer;
+        customer = new Customer();
+        customer.displayDetails();
+
+        Customer customer1 = new Customer(123245, "Ravan", "+91-21365", "abc@gamil.com", (byte) 35);
+        customer1.displayDetails();
+
+    }
+}
