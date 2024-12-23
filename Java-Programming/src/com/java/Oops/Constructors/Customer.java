012ac3d26fb591913a5c4e6f93732a35636377e9
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2024-12-23 22:00:43 +0530

    Constructors and static methods

---
+++ Java-Programming/src/com/java/Oops/Constructors/Customer.java
@@ -0,0 +1,47 @@
+package com.java.training.oops.constructors;
+
+public class Customer {
+    int id;
+    String name;
+    String contNo;
+    String email;
+    byte age;
+
+    final static String BRAND="Static is used for non changable data"; //Static Keyword
+
+/*
+    Constructor with out argument
+ */
+    Customer(){
+        id=12345;
+        name="Krish";
+        contNo="+91-12346587";
+        email="abcd@gmail.com";
+        age=21;
+        System.out.println("hello");
+    }
+
+    /*
+        Constuctor with argument
+     */
+
+    Customer(int id, String name, String contNo, String email, byte age) {
+        this.id = id;
+        this.name = name;  //This key word is to asign local variable to instance variables
+        this.contNo = contNo;
+        this.email = email;
+        this.age = age;
+    }
+    /*
+         Method to display details
+     */
+
+    void displayDetails(){
+        System.out.println("USing constructors ...........! ");
+        System.out.println("Id : "+id);
+        System.out.println("Name of the customer : "+name);
+
+        System.out.println("ContactNo : "+contNo);
+        System.out.println("Age : "+age);
+    }
+}
