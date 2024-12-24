1f093928cf0247d214fa86f021eca0919e3cc067
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmai.com>
2024-12-25 00:48:36 +0530

    Practice on Employee Management using constructors and methods

---
+++ Java-Programming/src/com/java/oops/constructors/Excersice/EmployeeManagement/Main.java
@@ -0,0 +1,9 @@
+package com.java.oops.constructors.Excersice.EmployeeManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee=new Employee("Krish",(byte)25,"abc@gmail.com","+91-235489","Developer",100000,10,10);
+        employee.displayInfo();
+
+    }
+}
