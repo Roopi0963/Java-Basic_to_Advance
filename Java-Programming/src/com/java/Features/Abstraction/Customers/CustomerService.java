6be33558f3cd6763b71e8814b1b2ff2472ff74bf
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2024-12-29 18:21:35 +0530

    Abstrsation class for customer

---
+++ Java-Programming/src/com/java/Features/Abstraction/Customers/CustomerService.java
@@ -0,0 +1,10 @@
+package com.java.Features.Abstraction.Customers;
+
+public abstract class CustomerService {
+
+
+
+    abstract void addCustomerToDb(CustomerI customerI);
+
+   // abstract void addCustomertoDb(Customer customer);
+}
