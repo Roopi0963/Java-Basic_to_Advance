0159c4ca832e693cb8b30b231a4c6ad69eb2ef0c
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2024-12-29 21:50:38 +0530

    Abstract class that extends animals

---
+++ Java-Programming/src/com/java/Features/Abstraction/Animals/Animal.java
@@ -0,0 +1,11 @@
+package com.java.Features.Abstraction.Animals;
+
+public abstract class Animal e
+    public Animal(String name, int age) {
+        super(name, age);
+    }
+
+    abstract void makeSound();
+
+    abstract void move();
+}
