07420e346482901455e74cc8abb7463f36f2aff2
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-01 08:23:44 +0530

    Airthmatic exceptionhandling using airthmatic operators

--- Java-Programming/src/com/java/ExceptionHandling/Airth.java
+++ Java-Programming/src/com/java/ExceptionHandling/Airth.java
@@ -1,4 +1,30 @@
 package com.java.ExceptionHandling;
 
 public class Airth {
+
+    int a;
+    int b;
+
+    public Airth(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int getDivisible(int a,int b){
+        try{
+            int res = a/b;
+            return res;
+        }catch (ArithmeticException e){
+            System.out.println("Please enter valid number");
+            return 0;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Airth airth=new Airth(8,0);
+        double re=airth.getDivisible(8,0);
+    }
+
 }
