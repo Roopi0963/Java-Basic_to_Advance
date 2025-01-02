7e2d684c005165e14700a910e09580f735e85846
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-02 21:38:47 +0530

    A class extends customer class and implements various methods

---
+++ Java-Programming/src/com/java/CollectionFrameworks/Collection/CustomerService.java
@@ -0,0 +1,8 @@
+package com.java.CollectionFrameworks;
+
+public class CustomerService extends Customer {
+    void displayDetails(Customer customer){
+        System.out.println("id : "+customer.getId());
+
+    }
+}
