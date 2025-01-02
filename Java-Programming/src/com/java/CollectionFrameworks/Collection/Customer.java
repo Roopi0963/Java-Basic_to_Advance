5d1e5a7ee63547676790ef3901c852a6b359e2e4
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-02 21:30:16 +0530

    Class with instances getter and setter methods

---
+++ Java-Programming/src/com/java/CollectionFrameworks/Collection/Customer.java
@@ -0,0 +1,71 @@
+package com.java.CollectionFrameworks;
+
+import java.util.Objects;
+
+public class Customer implements Comparable<Customer>{
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && age == customer.age && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public int compareTo(Customer o) {
+        return this.name.compareTo(getName());
+    }
+}
