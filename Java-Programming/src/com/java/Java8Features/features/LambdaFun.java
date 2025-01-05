3bad09f411b9e299b213aeb4ef60c6ff51f08e68
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-05 18:49:11 +0530

    Lambda Functions

--- Java-Programming/src/com/java/Java8Features/features/LambdaFun.java
+++ Java-Programming/src/com/java/Java8Features/features/LambdaFun.java
@@ -1,4 +1,37 @@
 package com.java.Java8Features.features;
 
-public class LambdaFun {
+public class lambdaFunction {
+    public static void main(String[] args) {
+        /*
+            1. Lambda Expression
+            2. It is the implementation for the functional interface(Instances of functional interface)
+            3. syntax
+         */
+
+        /*
+            Implementation
+
+            () -> {Logic}
+         */
+
+        FunInterface funInterface = () -> System.out.println("Functional interface implementing using lambda expression");
+        funInterface.getDetails();
+
+
+
+        /*
+            Using anonymous inner class
+         */
+        FunInterface funInterface1=new FunInterface() {
+            @Override
+            public void getDetails() {
+                System.out.println("Using anonymous inner class");
+            }
+        };
+        funInterface1.getDetails();
+
+
+
+
+    }
 }
