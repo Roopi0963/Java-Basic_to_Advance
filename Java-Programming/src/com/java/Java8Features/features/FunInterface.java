e2ef86a84f7004b1d5a1262ed21dae626a6c621a
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-01-05 18:50:15 +0530

    Functional Interfaces

---
+++ Java-Programming/src/com/java/Java8Features/features/FunInterface.java
@@ -0,0 +1,12 @@
+package com.java.Java8Features.features;
+/*
+    Functional interfaces are introduced to simplify anonymous inner class specially  in functional; programming
+ */
+
+@FunctionalInterface
+public interface FunInterface {
+    void getDetails();  // Single abstract method (SAM)
+                        // In this there  should be only one abstract method
+
+
+}
